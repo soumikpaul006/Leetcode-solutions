6db513df048bca5653fc900dbec7140cf43e9641
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-02-08 17:32:05 -0500

    Time: 92 ms (11.37%) | Memory: 101.7 MB (5.87%) - LeetSync

---
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals)
+    {
+
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->Integer.compare(a[1],b[1]));
+
+        //adding intervals by sorting based on first element of the interval;
+        for(int[] interval:intervals)
+        {
+            pq.offer(interval);
+        }
+
+        List<int[]> list=new ArrayList<>();
+
+        list.add(pq.peek());
+        pq.remove();
+
+        int count=0;
+        int i=0;
+        
+        while(!pq.isEmpty())
+        {   
+            int[] current_interval=list.get(i);
+            int[] new_interval=pq.remove();
+
+
+            int last_of_current_interval=current_interval[1];
+            int start_of_new_interval=new_interval[0];
+
+            if(start_of_new_interval<last_of_current_interval)
+            {
+                count++;
+
+            }else{
+                list.add(new_interval);
+                i++;
+            }
+        }
+
+        return count; 
+    }
+}
