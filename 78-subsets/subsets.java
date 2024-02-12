6b907296956259cb1c0dcaa7d4cac87048992647
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-02-11 21:58:30 -0500

    Time: 1 ms (68.89%) | Memory: 42.7 MB (49.92%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,25 @@
+class Solution 
+{   
+    public List<List<Integer>> subsets(int[] nums)
+    {
+        List<List<Integer>> final_list=new ArrayList<>();
+
+        final_list.add(new ArrayList<>());//add empty list
+
+        //iterate over each number
+        for(int num:nums)
+        {
+            int size=final_list.size();
+            for(int i=0;i<size;i++)
+            {
+                //make a copy the previous list
+                List<Integer> new_list=new ArrayList<>(final_list.get(i));
+                //add num to each list
+                new_list.add(num);
+                //add the new_list to the final_list
+                final_list.add(new_list);
+            }
+        }
+        return final_list;
+    }
+}
