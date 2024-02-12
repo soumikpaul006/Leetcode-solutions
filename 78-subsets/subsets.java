0e1c0b7f6fb035c7b74391cb6a1f2dc6efaf48df
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-02-12 16:50:31 -0500

    Time: 1 ms (68.76%) | Memory: 42.5 MB (65.39%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,27 +1,43 @@
 class Solution 
 {   
+    public List<List<Integer>> final_list=new ArrayList<>();
     public List<List<Integer>> subsets(int[] nums)
     {
-        List<List<Integer>> final_list=new ArrayList<>();
+        // List<List<Integer>> final_list=new ArrayList<>();
+        // final_list.add(new ArrayList<>());//add empty list
 
-        final_list.add(new ArrayList<>());//add empty list
+        // //iterate over each number
+        // for(int num:nums)
+        // {
+        //     int size=final_list.size();
+        //     for(int i=0;i<size;i++)
+        //     {
+        //         //make a copy the previous list
+        //         List<Integer> new_list=new ArrayList<>(final_list.get(i));
+        //         //add num to each list
+        //         new_list.add(num);
+        //         //add the new_list to the final_list
+        //         final_list.add(new_list);
+        //     }
+        // }
+        // return final_list;
 
-        //iterate over each number
-        for(int num:nums)
-        {
-            int size=final_list.size();
-            for(int i=0;i<size;i++)
-            {
-                //make a copy the previous list
-                List<Integer> new_list=new ArrayList<>(final_list.get(i));
-                //add num to each list
-                new_list.add(num);
-                //add the new_list to the final_list
-                final_list.add(new_list);
-            }
-        }
+        //backtracking approach
+        backtrack(nums,new ArrayList<>(),0);
         return final_list;
+
+    }
+    public void backtrack(int[] nums,List<Integer> new_list,int idx)
+    {
+        final_list.add(new ArrayList<>(new_list));
+
+        for(int i=idx;i<nums.length;i++)
+        {
+            new_list.add(nums[i]);
+            backtrack(nums,new_list,i+1);
+            new_list.remove(new_list.size()-1);
+        }
     }
 }
 //TC: O(n*2^n)
-//SC: O(n*2^n)
+//TcC: O(n*2^n)
