1a038a27613be5b75e9c87c4a677401c6f1b3167
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-02-22 19:25:36 -0500

    Time: 7 ms (5.85%) | Memory: 44.4 MB (5.97%) - LeetSync

---
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution
+{
+    class Pair
+    {
+        TreeNode node;
+        int idx;
+
+        Pair(TreeNode node,int idx)
+        {
+            this.node=node;
+            this.idx=idx;
+        }
+    }
+    public int widthOfBinaryTree(TreeNode root) 
+    {
+        if(root==null) return 0;
+        int max=Integer.MIN_VALUE;
+        Queue<Pair> q=new LinkedList<>();
+        List<List<Integer>> final_list=new LinkedList<>();
+
+        q.offer(new Pair(root,1));
+
+        while(!q.isEmpty())
+        {
+            int size=q.size();
+
+            List<Integer> list=new LinkedList<>();
+
+            int startIdx=q.peek().idx;
+            
+            for(int i=0;i<size;i++)
+            {
+                if(q.peek().node.left!=null)
+                {
+                    q.offer(new Pair(q.peek().node.left,2*(q.peek().idx-1)+1));
+                }
+                if(q.peek().node.right!=null)
+                {
+                    q.offer(new Pair(q.peek().node.right,2*(q.peek().idx-1)+2));
+                }
+
+                if(i==size-1)
+                {
+                    max=Math.max(max,q.peek().idx-startIdx+1);
+                }
+
+               
+
+                list.add(q.remove().node.val);
+            }
+
+            final_list.add(list);
+        }
+
+        System.out.print(final_list);
+
+        return max;
+    }
+}
