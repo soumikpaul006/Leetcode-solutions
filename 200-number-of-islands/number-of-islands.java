62d25fc47ccbbe1dad910415768711d8717ca8bc
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-02-25 20:48:00 -0500

    Time: 4 ms (51.51%) | Memory: 49.1 MB (80.71%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -3,15 +3,16 @@ class Solution
     public int numIslands(char[][] grid) 
     {
         int count=0;
+        boolean[][] vis=new boolean[grid.length][grid[0].length];
 
         for(int i=0;i<grid.length;i++)
         {
             for(int j=0;j<grid[i].length;j++)
             {
-                if(grid[i][j]=='1')
+                if(grid[i][j]=='1' && !vis[i][j])
                 {
                     count++;
-                    dfs(grid,i,j);
+                    dfs(grid,vis,i,j);
                 }
             }
         }
@@ -19,19 +20,20 @@ class Solution
         return count;
         
     }
-    public void dfs(char[][] grid,int i,int j)
+    public void dfs(char[][] grid,boolean[][] vis,int i,int j)
     {
         //base
-        if(i<0||j<0||i>grid.length-1||j>grid[0].length-1||grid[i][j]!='1')
+        if(i<0||j<0||i>grid.length-1||j>grid[0].length-1||grid[i][j]!='1'||vis[i][j])
         {
             return;
         }
 
-        grid[i][j]='2';
+        // grid[i][j]='2';
+        vis[i][j]=true;
 
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
+        dfs(grid,vis,i+1,j);
+        dfs(grid,vis,i-1,j);
+        dfs(grid,vis,i,j+1);
+        dfs(grid,vis,i,j-1);
     }
 }
