27ca9cd9c30ed6b94c320b55bc86c4e8089e858a
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-02-26 22:48:31 -0500

    Time: 11 ms (6.36%) | Memory: 46.5 MB (25.69%) - LeetSync

---
+++ 690-employee-importance/employee-importance.java
@@ -0,0 +1,58 @@
+/*
+// Definition for Employee.
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+};
+*/
+
+class Solution 
+{
+    int sum=0;
+    public int getImportance(List<Employee> employees, int id) 
+    {   
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+        
+        //mapping emp id with its imp
+        for(Employee e:employees)
+        {
+            map.put(e.id,e.importance);
+        }
+
+        dfs(employees,id,map);
+       
+        return sum;
+    }
+    public void dfs(List<Employee> emp, int id,HashMap<Integer,Integer> map)
+    {
+
+        // for(Employee e:emp)
+        // {
+        //     if(e.id==id)
+        //     {
+        //         sum+=map.get(id);
+        //     }
+        //     List<Integer> subord=e.subordinates;
+
+        //     for(Integer i:subord)
+        //     {
+        //         dfs(emp,i,map);
+        //     }
+        // }
+
+        for(Employee e : emp)
+        {
+            if(e.id == id)
+            {
+                sum += map.get(id);
+                List<Integer> subordinates = e.subordinates;
+                for(Integer i : subordinates)
+                {
+                    dfs(emp, i, map); // Recursively calculate importance of subordinates
+                }
+            }
+        }
+    }
+}
