48c6327c62affeb494105b34c261fb9e11f96afe
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-03-04 18:50:32 -0500

    Time: 2 ms (89.04%) | Memory: 45.5 MB (43.57%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -4,106 +4,90 @@ class Solution
     {   
         int row=board.length;
         int col=board[0].length;
-        
 
-        //applying dfs on all 4 edges
+        //Apply dfs at every edges
 
-        //1st col
-        for(int i=1;i<row-1;i++)
-        {
-            if(board[i][0]=='O')
-            {
-                dfs(board,i,0);
-            }
-        }
-
-        //last col
-        for(int i=1;i<row-1;i++)
-        {
-            if(board[i][col-1]=='O')
-            {
-                dfs(board,i,col-1);
-            }
-        }
-
-        //1st row
-        for(int i=1;i<col-1;i++)
+        //TOP ROW & BOTTOM ROW
+        for(int i=0;i<col;i++)
         {
             if(board[0][i]=='O')
             {
                 dfs(board,0,i);
             }
-        }
-
-        //last row
-        for(int i=1;i<col-1;i++)
-        {
             if(board[row-1][i]=='O')
             {
                 dfs(board,row-1,i);
             }
         }
 
+        //LEFT COLUMN AND RIGHT COLUMN
+        for(int i=0;i<row;i++)
+        {
+            if(board[i][0]=='O')
+            {
+                dfs(board,i,0);
+            }
+            if(board[i][col-1]=='O')
+            {
+                dfs(board,i,col-1);
+            }
+        }
 
-        
-        //convert all the Y-> O and O->X;
+        //covert O-> X and Y-> O in the middle of the matrix
+
         for(int i=1;i<row-1;i++)
         {
             for(int j=1;j<col-1;j++)
             {
+                if(board[i][j]=='O')
+                {
+                    board[i][j]='X';
+                }
                 if(board[i][j]=='Y')
                 {
                     board[i][j]='O';
                 }
-                else if(board[i][j]=='O')
-                {
-                    board[i][j]='X';
-                }
             }
         }
 
-        //convert the Y->O in both the edge rows 
+        //Converting Ys in the edges O
+        for(int i=0;i<col;i++)
+        {
+            if(board[0][i]=='Y')
+            {
+                board[0][i]='O';
+            }
+            if(board[row-1][i]=='Y')
+            {
+                board[row-1][i]='O';
+            }
+        }
+
+        //Converting Ys in the edges O
         for(int i=0;i<row;i++)
-        {   
+        {
             if(board[i][0]=='Y')
-            {   
+            {
                 board[i][0]='O';
             }
             if(board[i][col-1]=='Y')
             {
                 board[i][col-1]='O';
             }
-        }
-
-
-        //convert the Y->O in both the edge cols 
-        for(int j=0;j<col;j++)
-        {
-            if(board[0][j]=='Y')
-            {
-                board[0][j]='O';
-            }
-            if(board[row-1][j]=='Y')
-            {
-                board[row-1][j]='O';
-            }
-        }
-
-  
+        } 
     }
-    public void dfs(char[][] grid, int i,int j)
+    public void dfs(char[][] board,int i,int j)
     {
-        //base
-        if(i<0||j<0||i>grid.length-1||j>grid[0].length-1||grid[i][j]!='O')
+        if(i<0||j<0||i>board.length-1||j>board[0].length-1||board[i][j]!='O')
         {
             return;
         }
 
-        grid[i][j]='Y';
+        board[i][j]='Y';
 
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
+        dfs(board,i+1,j);
+        dfs(board,i-1,j);
+        dfs(board,i,j+1);
+        dfs(board,i,j-1);
     }
 }
