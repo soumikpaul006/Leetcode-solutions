8dd26636aacfcad590060a88fb63f3198fe25ea1
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-03-04 23:28:59 -0500

    Time: 9 ms (61.88%) | Memory: 56.6 MB (59.35%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -58,14 +58,14 @@ class Solution {
             }
         }
 
-        for(int i=0;i<row;i++)
-        {
-            for(int j=0;j<col;j++)
-            {
-                System.out.print(board[i][j]+" ");
-            }
-            System.out.println();
-        }
+        // for(int i=0;i<row;i++)
+        // {
+        //     for(int j=0;j<col;j++)
+        //     {
+        //         System.out.print(board[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
 
         return count;
     }
