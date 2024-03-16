dce7dab8187166a680b87e4266edae3fb857b2df
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-03-15 21:25:29 -0400

    Time: 7 ms (43.15%) | Memory: 46.2 MB (10.87%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,85 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) 
+    {
+        //Detect a Cycle in a Directed Graph
+        //Topological sort BFS
+
+
+        //form a adj_list
+
+        ArrayList<ArrayList<Integer>> adj_list=new ArrayList<>();
+
+        for(int i=0;i<numCourses;i++)
+        {
+            adj_list.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<prerequisites.length;i++)
+        {
+            adj_list.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+
+        //in degree
+        int[] indegree=new int[numCourses];
+
+        for(int i=0;i<numCourses;i++)
+        {
+            for(Integer course:adj_list.get(i))
+            {
+                indegree[course]++;
+            }
+        }
+        
+
+        Queue<Integer> q=new LinkedList<>();
+
+        //add indegree 0 to the q first
+        for(int i=0;i<numCourses;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.offer(i);
+            }
+        }
+
+        List<Integer> list=new ArrayList<>();
+
+        while(!q.isEmpty())
+        {
+            int node=q.remove();
+            list.add(node);
+
+            for(int neighbors:adj_list.get(node))
+            {
+                indegree[neighbors]--;
+
+                if(indegree[neighbors]==0)
+                {
+                    q.offer(neighbors);
+                }  
+
+            }
+        }
+
+        boolean flag=true;
+
+        if(list.size()!=numCourses)
+        {
+            flag=false;
+        }
+
+        if(!flag)
+        {
+            return new int[0];
+        }
+
+        int[] arr=new int[list.size()];
+
+        for(int i=0;i<list.size();i++)
+        {
+            arr[i]=list.get(i);
+        }
+
+        return arr;
+    }
+}
