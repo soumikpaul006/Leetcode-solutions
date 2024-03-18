bed15b56ae664254af51c792fa92e47bf771efaf
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-03-18 13:32:30 -0400

    Time: 83 ms (9.81%) | Memory: 76.9 MB (10.14%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -2,33 +2,37 @@ class Solution {
     public int findMinArrowShots(int[][] points) 
     {   
         PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->Integer.compare(a[1],b[1]));
-        int count=1;
 
-        for(int[] interval:points)
+        for(int[] point:points)
         {
-            pq.add(interval);
-        } 
+            pq.add(point);
+        }
 
         List<int[]> list=new ArrayList<>();
         list.add(pq.remove());
 
+        int count=1;
         int i=0;
+
         while(!pq.isEmpty())
         {
             int[] current_interval=list.get(i);
             int[] new_interval=pq.remove();
 
+            int start_current_interval=current_interval[0];
             int end_current_interval=current_interval[1];
             int start_new_interval=new_interval[0];
+            int end_new_interval=new_interval[1];
 
             if(start_new_interval>end_current_interval)
             {
                 count++;
                 list.add(new_interval);
                 i++;
-            }   
+            }
         }
-
         return count;
+
+        
     }
 }
