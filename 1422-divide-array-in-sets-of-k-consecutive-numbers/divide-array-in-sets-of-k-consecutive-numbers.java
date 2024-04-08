862c6e9ca31e0626976f5584b1e97026411e888e
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-04-08 16:11:10 -0400

    Time: 46 ms (89.56%) | Memory: 56.3 MB (74.95%) - LeetSync

---
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean isPossibleDivide(int[] hand, int groupSize) 
+    {
+
+        Arrays.sort(hand);
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        for(int i=0;i<hand.length;i++)
+        {
+            map.put(hand[i],map.getOrDefault(hand[i],0)+1);
+        }
+
+        for(int i=0;i<hand.length;i++)
+        {
+
+            int num=hand[i];
+            int size=groupSize;
+
+            // Add this check to make sure we're not starting a new group unless the current group is terminated
+            //all occurance of num is is already used in making groups; map.get(num)==0;
+            if (!map.containsKey(num))
+                continue;
+
+            while (size > 0) 
+            {
+                // If the current group can't be formed, return false
+                //if the next value is not present then we still have room to add num then reture false;
+                if (!map.containsKey(num))
+                    return false; 
+                
+                // Decrement the count of cards in the group
+                map.put(num, map.get(num) - 1);
+
+                if (map.get(num) == 0) {
+                    map.remove(num);
+                }
+
+                num++;
+                size--;
+            }
+
+        }
+
+
+        return map.size()==0?true:false;
+        
+    }
+}
