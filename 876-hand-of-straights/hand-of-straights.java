5a478a3fa061e5961f10004db740b572af0d1f48
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-04-08 16:10:02 -0400

    Time: 26 ms (84.25%) | Memory: 45.8 MB (30.17%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -19,13 +19,16 @@ class Solution
             int size=groupSize;
 
             // Add this check to make sure we're not starting a new group unless the current group is terminated
+            //all occurance of num is is already used in making groups; map.get(num)==0;
             if (!map.containsKey(num))
                 continue;
 
             while (size > 0) 
             {
+                // If the current group can't be formed, return false
+                //if the next value is not present then we still have room to add num then reture false;
                 if (!map.containsKey(num))
-                    return false; // If the current group can't be formed, return false
+                    return false; 
                 
                 // Decrement the count of cards in the group
                 map.put(num, map.get(num) - 1);
