5b1eb843dbdb5716700d32654451e9e8593bd1f9
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-04-09 14:22:37 -0400

    Time: 0 ms (100.00%) | Memory: 41.2 MB (47.55%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -6,26 +6,40 @@ class Solution {
 
         int count=0;
 
-        while(tickets[k]>0)
-        {
-            for(int i=0;i<tickets.length;i++)
-            {
-                if(tickets[i]>0)
-                {
-                    tickets[i]--;
-                    count++;
-                }
-                
-                if(tickets[k]==0){
-                    return count;
-                }
-            }
-            
-        }
+        //O(n^2)
+        //Brute Force
+        // while(tickets[k]>0)
+        // {
+        //     for(int i=0;i<tickets.length;i++)
+        //     {
+        //         if(tickets[i]>0)
+        //         {
+        //             tickets[i]--;
+        //             count++;
+        //         }
 
+        //         if(tickets[k]==0){
+        //             return count;
+        //         }
+        //     }
+            
+        // }
+
+        // for(int i=0;i<tickets.length;i++)
+        // {
+        //     System.out.print(tickets[i]+" ");
+        // }
+
+        //O(n)
+        //Optimised
         for(int i=0;i<tickets.length;i++)
         {
-            System.out.print(tickets[i]+" ");
+            if(i<=k)
+            {
+                count+=Math.min(tickets[i],tickets[k]);
+            }else{
+                count+=Math.min(tickets[i],tickets[k]-1);
+            }
         }
 
         return count;
