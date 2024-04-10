f38013ec2bdcc57547a92ad5c4c2da9181422821
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-04-10 16:33:00 -0400

    Time: 0 ms (100.00%) | Memory: 41.2 MB (68.24%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -19,53 +19,90 @@ class Solution
     {
 
         // TC: O(n) ; SC: O(n)
-        Stack<Pair> open_stack = new Stack<>();
-        Stack<Pair> star_stack = new Stack<>();
+        // Stack<Pair> open_stack = new Stack<>();
+        // Stack<Pair> star_stack = new Stack<>();
 
+
+        // for(int i=0;i<s.length();i++)
+        // {
+
+        //     if(s.charAt(i)=='(')
+        //     {
+        //         open_stack.add(new Pair('(',i));
+
+        //     }else if(s.charAt(i)=='*'){
+
+        //         star_stack.add(new Pair('*',i));
+        //     }
+
+
+        //     else if(s.charAt(i)==')')
+        //     {
+        //         if(!open_stack.isEmpty())
+        //         {
+        //             open_stack.pop();
+        //         }
+
+        //         else if(!star_stack.isEmpty())
+        //         {
+        //             star_stack.pop();
+        //         }
+        //         else
+        //         {
+        //             return false;
+        //         }            
+        //     }
+        // }
+
+        // while(!open_stack.isEmpty())
+        // {
+        //     if(!star_stack.isEmpty() && (open_stack.peek().second<star_stack.peek().second) )
+        //     {
+        //         open_stack.pop();
+        //         star_stack.pop();
+
+        //     }else{
+        //         return false;
+        //     }
+        // }
+        
+        // return open_stack.isEmpty();  
+
+
+        //Optimised
+        // TC: O(n); SC: TC: O(1);
+
+        int open_count=0;
+        int close_count=0;
+
+        int len=s.length()-1;
 
         for(int i=0;i<s.length();i++)
         {
 
-            if(s.charAt(i)=='(')
+            //considering * as a (
+            if(s.charAt(i)=='(' || s.charAt(i)=='*')
             {
-                open_stack.add(new Pair('(',i));
-
-            }else if(s.charAt(i)=='*'){
-
-                star_stack.add(new Pair('*',i));
-            }
-
-
-            else if(s.charAt(i)==')')
-            {
-                if(!open_stack.isEmpty())
-                {
-                    open_stack.pop();
-                }
-
-                else if(!star_stack.isEmpty())
-                {
-                    star_stack.pop();
-                }
-                else
-                {
-                    return false;
-                }            
-            }
-        }
-
-        while(!open_stack.isEmpty())
-        {
-            if(!star_stack.isEmpty() && (open_stack.peek().second<star_stack.peek().second) )
-            {
-                open_stack.pop();
-                star_stack.pop();
-
+                open_count++;
             }else{
-                return false;
+                open_count--;
             }
+
+
+            //considering * as a )
+            if(s.charAt(len-i)==')' || s.charAt(len-i)=='*')
+            {
+                close_count++;
+            }else{
+                close_count--;
+            }
+
+
+            if(open_count<0 || close_count<0) return false;
+
         }
-        
-        return open_stack.isEmpty();  
+
+        return true;
     }
+
 }
