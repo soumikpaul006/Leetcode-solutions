4ae94da5f6e933ae70a91d492c0d94a9efd09b02
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-04-14 19:55:26 -0400

    Time: 8 ms (76.71%) | Memory: 44.9 MB (93.54%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,21 +1,27 @@
-public class Solution {
-    public int minDeletions(String s) {
-        HashMap<Character, Integer> cnt = new HashMap<>();
-        int deletions = 0;
-        HashSet<Integer> used_frequencies = new HashSet<>();
-        
-        for (char c : s.toCharArray()) {
-            cnt.put(c, cnt.getOrDefault(c, 0) + 1);
+class Solution {
+    public int minDeletions(String s) 
+    {
+        int[] freq=new int[26];
+
+        for(int i=0;i<s.length();i++)
+        {
+            freq[s.charAt(i)-'a']++;
         }
-        
-        for (int freq : cnt.values()) {
-            while (freq > 0 && used_frequencies.contains(freq)) {
-                freq--;
-                deletions++;
+
+        HashSet<Integer> set=new HashSet<>();
+
+        int del=0;
+
+        for(int i=0;i<freq.length;i++)
+        {
+            while(freq[i]!=0 && !set.add(freq[i]))
+            {
+                freq[i]--;
+                del++;
+
             }
-            used_frequencies.add(freq);
         }
         
-        return deletions;
+        return del;
     }
 }
