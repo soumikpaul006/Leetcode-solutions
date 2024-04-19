cbbd2216e74e8c1adea42a4399c395ceb622dbb3
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-04-19 02:43:33 -0400

    Time: 0 ms (100.00%) | Memory: 43.2 MB (34.70%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -8,16 +8,17 @@ class Solution
     }
     public int helper(int[] cost,int n)
     {
-        int[] dp=new int[n];
 
-        dp[0]=cost[0];
-        dp[1]=cost[1];
+        int prev1=cost[0];
+        int prev2=cost[1];
 
         for(int i=2;i<n;i++)
         {
-            dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
+            int curr=cost[i]+Math.min(prev1,prev2);
+            prev1=prev2;
+            prev2=curr;
         }
         
-        return Math.min(dp[n-1],dp[n-2]);
+        return Math.min(prev1,prev2);
     }
 }
