f1139f849a9fcee379acfac63860938e9583be32
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-04-21 19:06:33 -0400

    Time: 119 ms (41.15%) | Memory: 46.1 MB (18.22%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -2,46 +2,56 @@ class Solution
 {
     public String longestPalindrome(String s) 
     {
-        if (s.length() <= 1) return s;
 
-        int maxLen = 0;
-        int start = 0;
-        int end = 0;
 
-        for (int i = 0; i < s.length(); i++) 
+        if(s.length()==1) return s;
+        int len=s.length();
+        boolean[][] dp=new boolean[len][len];
+
+        //1
+        for(int i=0;i<len;i++)
         {
-            for (int j = i; j < s.length(); j++) 
+            dp[i][i]=true;
+        }
+
+
+        int start=0;
+        int end=0;
+
+        //2
+        for(int i=0;i<len-1;i++)
+        {
+            if(s.charAt(i)==s.charAt(i+1))
             {
-                if (isPalindrome(s, i, j)) 
+                dp[i][i+1]=true;
+                start=i;
+                end=i+1;
+            }
+        }
+
+
+        //2 and more gap
+        for(int gap=2;gap<len;gap++)
+        {
+            for(int j=0;j<len-gap;j++)
+            {
+                int a=j;
+                int b=a+gap;
+
+                if(s.charAt(a)==s.charAt(b) && dp[a+1][b-1]==true)
                 {
-                    int len = j - i + 1;
-                    if (len > maxLen)
+                    dp[a][b]=true;
+
+                    if(b-a>end-start)
                     {
-                        maxLen = len;
-                        start = i;
-                        end = j;
+                        start=a;
+                        end=b;
                     }
                 }
             }
         }
 
-        return s.substring(start, end + 1);
-    }
-
-    public boolean isPalindrome(String s, int start,int end)
-    {
-        while(start<=end)
-        {
-            if(s.charAt(start)!=s.charAt(end))
-            {
-                return false;
-            }
-            else{
-                start++;
-                end--;
-            }
-        }
-
-        return true;
+        return s.substring(start,end+1);
+ 
     }
 }
