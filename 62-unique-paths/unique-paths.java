d2ce5dd8f5f8a6e9cc8869916562b1a2a79b6859
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-04-26 16:22:47 -0400

    Time: 0 ms (100.00%) | Memory: 40.1 MB (81.45%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,30 @@
+class Solution 
+{
+    //O(2^m*n);
+    public int uniquePaths(int m, int n) 
+    {
+        int[][] dp=new int[m][n];
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                dp[i][j]=-1;
+            }
+        }
+        return helper(m-1,n-1,dp);
+    }
+    public int helper(int i,int j,int[][] dp)
+    {
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+       
+        
+        int up=helper(i-1,j,dp);
+        int left=helper(i,j-1,dp);
+
+        return dp[i][j]=up+left;
+    }
+}
