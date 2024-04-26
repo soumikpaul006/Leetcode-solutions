f6e06365130714458182fb9c1101774d59364050
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-04-26 17:43:08 -0400

    Time: 0 ms (100.00%) | Memory: 41.7 MB (34.83%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -7,30 +7,38 @@ class Solution
 
         if(obstacleGrid[0][0]==1) return 0;
 
-        int[][] dp=new int[m][n];
+        return helper(obstacleGrid,m,n);
+    }
+    public int helper(int[][] obstacleGrid,int m,int n)
+    {
+        int[][] dp = new int[m][n];
 
-        for(int i=0;i<m;i++)
+        dp[0][0] = (obstacleGrid[0][0] == 1) ? 0 : 1;
+
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = (obstacleGrid[i][0] == 1) ? 0 : dp[i - 1][0];
+        }
+
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = (obstacleGrid[0][j] == 1) ? 0 : dp[0][j - 1];
+        }
+
+
+        for (int i = 1; i < m; i++) 
         {
-            for(int j=0;j<n;j++)
+            for (int j = 1; j < n; j++) 
             {
-                dp[i][j]=-1;
+                if (obstacleGrid[i][j] == 1) 
+                {
+                    dp[i][j] = 0; 
+                } 
+                else 
+                {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]; 
+                }
             }
         }
 
-        return helper(obstacleGrid,m-1,n-1,dp);
-    }
-    public int helper(int[][] obstacleGrid,int m,int n,int[][] dp)
-    {
-        if(m==0 && n==0) return 1;
-        if(m<0 || n<0) return 0;
-
-        if(obstacleGrid[m][n]==1) return 0;
-
-        if(dp[m][n]!=-1) return dp[m][n];
-
-        int up=helper(obstacleGrid,m-1,n,dp);
-        int left=helper(obstacleGrid,m,n-1,dp);
-
-        return dp[m][n]=up+left;
+        return dp[m - 1][n - 1];
     }
 }
