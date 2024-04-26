e05477562fea41dd2b809639314efa1972626264
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-04-26 17:53:22 -0400

    Time: 0 ms (100.00%) | Memory: 41.4 MB (72.63%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -13,28 +13,32 @@ class Solution
     {
         int[][] dp = new int[m][n];
 
-        dp[0][0] = (obstacleGrid[0][0] == 1) ? 0 : 1;
+        dp[0][0]=1;
 
-        for (int i = 1; i < m; i++) {
-            dp[i][0] = (obstacleGrid[i][0] == 1) ? 0 : dp[i - 1][0];
-        }
-
-        for (int j = 1; j < n; j++) {
-            dp[0][j] = (obstacleGrid[0][j] == 1) ? 0 : dp[0][j - 1];
-        }
-
-
-        for (int i = 1; i < m; i++) 
+        for (int i = 0; i < m; i++) 
         {
-            for (int j = 1; j < n; j++) 
+            for (int j = 0; j < n; j++) 
             {
-                if (obstacleGrid[i][j] == 1) 
+                if(obstacleGrid[i][j] == 1) 
                 {
                     dp[i][j] = 0; 
                 } 
+                else if(i==0 && j==0 ) dp[0][0]=1;
                 else 
                 {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]; 
+                    int up=0;
+                    int left=0;
+
+                    if(i>0)
+                    {
+                        up=dp[i - 1][j];
+                    } 
+                    if(j>0)
+                    {
+                        left=dp[i][j - 1];
+                    } 
+
+                    dp[i][j] =  up + left; 
                 }
             }
         }
