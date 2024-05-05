a164191e756facccb3aad1483f3c5001dd4a3de8
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-05-05 13:27:46 -0400

    Time: 3 ms (65.52%) | Memory: 44 MB (52.58%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -2,32 +2,27 @@ class Solution
 {
     public int minimumTotal(List<List<Integer>> triangle) 
     {
+
+        if(triangle.size()==1) return triangle.get(0).get(0);
+
         int[][] dp=new int[triangle.size()][triangle.size()];
 
-        for(int i=0;i<dp.length;i++)
+        //base
+        for(int i=0;i<triangle.size();i++)
+        {   
+            //assign the last row of dp == last row of triangle as it is
+            dp[triangle.size()-1][i]=triangle.get(triangle.size()-1).get(i);
+        }
+
+        for(int i=triangle.size()-2;i>=0;i--)
         {
-            for(int j=0;j<dp[i].length;j++)
+            for(int j=0;j<=i;j++)
             {
-                dp[i][j]=-1;
+                dp[i][j]=Math.min(dp[i+1][j],dp[i+1][j+1])+triangle.get(i).get(j);
             }
         }
 
-        return helper(triangle,0,0,dp);
+        return dp[0][0];
     }
-    public int helper(List<List<Integer>> triangle,int i,int j,int[][] dp)
-    {
-        //base
-        if(i==triangle.size()-1)
-        {
-            return triangle.get(i).get(j);
-        }
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int one=helper(triangle,i+1,j,dp);
-        int two=helper(triangle,i+1,j+1,dp);
-
-        return dp[i][j]=Math.min(one,two)+triangle.get(i).get(j);
-
-    }
+    
 }
