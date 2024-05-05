667fd1d840b51a94c8af0fbe0bf1fdbf58ffee14
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-05-05 18:41:30 -0400

    Time: 8 ms (34.53%) | Memory: 45.4 MB (34.66%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,54 +1,48 @@
 class Solution {
-    public int minFallingPathSum(int[][] matrix) 
+    public int minFallingPathSum(int[][] num) 
     {
+        //base
+        int[][] dp=new int[num.length][num.length];
+
+        for(int i=0;i<num.length;i++)
+        {
+            dp[num.length-1][i]=num[num.length-1][i]; //asign the last row of the dp as the last row of the num
+        }
+
+        for(int i=num.length-2;i>=0;i--)
+        {
+            for(int j=0;j<num.length;j++)
+            {
+                int one=Integer.MAX_VALUE;
+                if(j>0)
+                {
+                    one=dp[i+1][j-1]; //left diagonal
+                }
+
+                int two=dp[i+1][j]; //down
+
+                int three=Integer.MAX_VALUE;
+
+                if(j<num[0].length-1)
+                {
+                    three=dp[i+1][j+1]; //right diagonal
+                }
+
+                dp[i][j]=Math.min(one,Math.min(two,three))+num[i][j];
+
+            }
+        }
+
+
         int min=Integer.MAX_VALUE;
 
-        Integer[][] dp=new Integer[matrix.length][matrix[0].length];
-
-        // for(int i=0;i<dp.length;i++)
-        // {
-        //     for(int j=0;j<dp[i].length;j++)
-        //     {
-        //         dp[i][j]=-1;
-        //     }
-        // }
-
-		for(int i=0;i<matrix[0].length;i++)
+		for(int i=0;i<num[0].length;i++)
 		{
-			int temp_min=helper(matrix,0,i,dp);
-			min=Math.min(min,temp_min);
+			min=Math.min(min,dp[0][i]);
 		}
 
 		return min;
         
     }
-    public static int helper(int[][] num,int i,int j,Integer[][] dp)
-	{
-		//base
-		if(i==num.length-1)
-		{
-			return num[i][j];
-		}
 
-        if(dp[i][j]!=null) return dp[i][j];
-
-		int one=Integer.MAX_VALUE;
-
-		if(j>0)
-		{
-			one=helper(num,i+1,j-1,dp); //left diagonal
-		}
-		
-		int two=helper(num,i+1,j,dp); //down
-
-		int three=Integer.MAX_VALUE;
-
-		if(j<num[0].length-1)
-		{
-			three=helper(num,i+1,j+1,dp); //right diagonal
-		}
-
-		return dp[i][j]=Math.min(one,Math.min(two,three))+num[i][j];
-
-	} 
 }
