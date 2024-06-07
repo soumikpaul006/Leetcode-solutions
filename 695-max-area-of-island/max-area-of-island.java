306cbb4436be925e94686da2effc391e49a8557c
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-06-07 11:54:30 -0400

    Time: 1 ms (100.00%) | Memory: 44.5 MB (42.55%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,41 +1,44 @@
-class Solution 
-{   
-   
+class Solution {
+
     public int maxAreaOfIsland(int[][] grid) 
     {
         int row=grid.length;
         int col=grid[0].length;
-        int ans=0; 
+
+        int ans=0;
+
         for(int i=0;i<row;i++)
         {
             for(int j=0;j<col;j++)
             {
                 if(grid[i][j]==1)
                 {
-                    int sum=dfs(grid,i,j,0);   
-                    ans=Math.max(ans,sum);
+                    int count=dfs(grid,i,j,0);
+                    ans=Math.max(ans,count);
                 }
             }
         }
-    
+
         return ans;
+        
     }
     public int dfs(int[][] grid,int i,int j,int sum)
     {
-
+        //base
         if(i<0||j<0||i>grid.length-1||j>grid[0].length-1||grid[i][j]!=1)
         {
             return sum;
         }
-
         sum=sum+1;
-        
+
         grid[i][j]=2;
 
-        sum=dfs(grid,i+1,j,sum);
-        sum=dfs(grid,i-1,j,sum);
-        sum=dfs(grid,i,j+1,sum);
-        sum=dfs(grid,i,j-1,sum);
+        
+
+        sum= dfs(grid,i+1,j,sum);
+        sum= dfs(grid,i-1,j,sum);
+        sum= dfs(grid,i,j+1,sum);
+        sum= dfs(grid,i,j-1,sum);
 
         return sum;
 
