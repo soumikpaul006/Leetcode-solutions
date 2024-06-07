0c6fb0aeb9def1da5e7ee1ca4a9f767c8ab505de
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-06-07 12:18:18 -0400

    Time: 2 ms (73.48%) | Memory: 42.6 MB (19.59%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,73 +1,70 @@
-class Solution 
-{
-    class Pair
+class Pair{
+    int row;
+    int col;
+    int time;
+
+    Pair(int row,int col,int time)
     {
-        int row;
-        int col;
-        int time;
-
-        Pair(int row,int col,int time)
-        {
-            this.row=row;
-            this.col=col;
-            this.time=time;
-        }
-
+        this.row=row;
+        this.col=col;
+        this.time=time;
     }
+}
+class Solution {
     public int orangesRotting(int[][] grid) 
     {
-        int n=grid.length;
-        int m=grid[0].length;
+        int row=grid.length;
+        int col=grid[0].length;
 
-        int[][] vis=new int[n][m];
+        int[][] vis=new int[row][col];
 
         Queue<Pair> q=new LinkedList<>();
 
         int countFresh=0;
 
-        for(int i=0;i<n;i++)
+        for(int i=0;i<row;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<col;j++)
             {
                 if(grid[i][j]==2)
                 {
                     q.offer(new Pair(i,j,0));
                     vis[i][j]=2;
                 }
-                else
-                {
+                else{
                     vis[i][j]=0;
                 }
-                
+
                 if(grid[i][j]==1)
                 {
                     countFresh++;
                 }
             }
         }
-        
-        int[] delrow={-1,0,+1,0};
-        int[] delcol={0,-1,0,+1};
+
+        int[] delrow={+1,0,-1,0};
+        int[] delcol={0,+1,0,-1};
+
+        int ans=0;
 
         int count=0;
-        int max=0;
 
         while(!q.isEmpty())
         {
-            int row=q.peek().row;
-            int col=q.peek().col;
+            int r=q.peek().row;
+            int c=q.peek().col;
             int tmt=q.peek().time;
 
             q.remove();
 
-            max=Math.max(max,tmt);
+            ans=Math.max(ans,tmt);
 
             for(int i=0;i<4;i++)
             {
-                int nrow=row+delrow[i];
-                int ncol=col+delcol[i];
+                int nrow=r+delrow[i];
+                int ncol=c+delcol[i];
 
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]==0)
+                if(nrow>=0 && ncol>=0 && nrow<row && ncol<col && grid[nrow][ncol]==1 && vis[nrow][ncol]==0)
                 {
                     q.offer(new Pair(nrow,ncol,tmt+1));
                     vis[nrow][ncol]=2;
@@ -78,6 +75,7 @@ class Solution
 
         if(count!=countFresh) return -1;
 
-        return max;
+        return ans;
+   
     }
 }
