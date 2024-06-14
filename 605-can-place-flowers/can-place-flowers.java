e7b27ddd80783a4a2c2943afdb46c147028b689a
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-06-14 13:36:16 -0400

    Time: 2 ms (8.09%) | Memory: 45.4 MB (56.42%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -5,7 +5,7 @@ class Solution {
 
         for(int i=0;i<num.length;i++)
         {
-            if(num[i]==0 && (i==0||num[i-1]==0) &&  (i==num.length-1||num[i+1]==0))
+            if((i==0||num[i-1]==0) && num[i]==0 &&  (i==num.length-1||num[i+1]==0))
             {
                 num[i]=1;
                 n--;
