19343a230e908287a270710ec10086244d11ef55
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-06-19 16:23:14 -0400

    Time: 1 ms (99.55%) | Memory: 56.9 MB (47.87%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,48 +1,17 @@
 class Solution {
     public int maxSubArray(int[] nums) 
     {
-        //Brute Force
-
-        // int ans=nums[0];
-
-        // for(int i=0;i<nums.length;i++)
-        // {
-        //     int max=nums[i];
-        //     ans=Math.max(ans,max);
-
-        //     for(int j=i+1;j<nums.length;j++)
-        //     {
-        //         max=max+nums[j];
-        //         ans=Math.max(ans,max);
-        //     }
-            
-        // }
-
-        // return ans;
-
-
-
-        //Kadan's Algorithm
-
         if(nums.length==1) return nums[0];
 
-        int max=nums[0];
-        int curr_max=nums[0];
+        int totalMax=nums[0];
+        int max=nums[0]; 
 
         for(int i=1;i<nums.length;i++)
-        {
-                // if(nums[i]<nums[i]+curr_max)
-                // {
-                //     curr_max=curr_max+nums[i];
-                // }
-                // else{
-                //     curr_max=nums[i];
-                // }
-                
-                curr_max=Math.max(nums[i],curr_max+nums[i]);
-                max=Math.max(curr_max,max);
+        {   
+            max=Math.max(nums[i]+max,nums[i]);
+            totalMax=Math.max(totalMax,max);
         }
 
-        return max;
+        return totalMax;
     }
 }
