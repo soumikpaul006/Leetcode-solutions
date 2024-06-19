3040780fe6c6e9713309c1a61efa64c8362c19aa
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-06-19 00:31:12 -0400

    Time: 1 ms (81.10%) | Memory: 41.6 MB (58.21%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -3,38 +3,33 @@ class Solution
 
     public int maxProfit(int[] prices) 
     {
-        int[][] dp = new int[prices.length][2]; // 2 states: 0 for holding, 1 for not holding
+        int n=prices.length;
+        int[][] dp = new int[prices.length+2][2]; // 2 states: 0 for holding, 1 for not holding
 
-        for (int i = 0; i < dp.length; i++) {
-            dp[i][0] = -1;
-            dp[i][1] = -1;
+        dp[n][0]=0;
+        dp[n][1]=0;
+
+        int profit=0;
+
+        for(int idx=n-1;idx>=0;idx--)
+        {
+            for(int buy=0;buy<=1;buy++)
+            {
+                if (buy == 1) 
+                {
+                    // Buy or do not buy
+                    profit = Math.max(-prices[idx] + dp[idx + 1][0], // buy
+                                    0 + dp[idx + 1][1]); // do not buy
+                } else {
+                    // Sell or do not sell
+                    profit = Math.max(prices[idx] + dp[idx + 2][1], // sell
+                                    0 + dp[idx + 1][0]); // do not sell
+                }
+                dp[idx][buy]=profit;
+            }
+            
         }
 
-        return func(prices, 0, 1, dp); // Starting with index 0 and buy allowed
+        return dp[0][1]; 
     }
-   public int func(int[] prices, int idx, int buy, int[][] dp) {
-        // base case
-        if (idx >= prices.length) {
-            return 0;
-        }
-
-        if (dp[idx][buy] != -1) {
-            return dp[idx][buy];
-        }
-
-        int profit = 0;
-        if (buy == 1) {
-            // Buy or do not buy
-            profit = Math.max(-prices[idx] + func(prices, idx + 1, 0, dp), // buy
-                              0 + func(prices, idx + 1, 1, dp)); // do not buy
-        } else {
-            // Sell or do not sell
-            profit = Math.max(prices[idx] + func(prices, idx + 2, 1, dp), // sell
-                              0 + func(prices, idx + 1, 0, dp)); // do not sell
-        }
-
-        
-        return dp[idx][buy] = profit;
-    }
-
 }
