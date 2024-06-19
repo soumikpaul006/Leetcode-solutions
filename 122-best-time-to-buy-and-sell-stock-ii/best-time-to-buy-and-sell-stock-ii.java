3b06f4f6c718b92d6746367c033beb2610c0910c
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-06-18 20:18:18 -0400

    Time: 3 ms (11.07%) | Memory: 49.6 MB (5.13%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,26 +1,37 @@
-class Solution 
-{
-    public int maxProfit(int[] prices) 
-    {
-        int max=0;
-        int start=prices[0];
+class Solution {
+    public int maxProfit(int[] prices) {
+        int[][] dp = new int[prices.length][2]; // 2 states: 0 for holding, 1 for not holding
 
-        for(int i=1;i<prices.length;i++)
-        {
-
-                if(prices[i]<start)
-                {
-                    start=prices[i];
-                }
-
-                if(prices[i]>start)
-                {
-                    max+=prices[i]-start;
-                    start=prices[i];
-                }
-                
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = -1;
+            dp[i][1] = -1;
         }
 
-        return max;
+        return func(prices, 0, 1, dp); // Starting with index 0 and buy allowed
+    }
+
+    public int func(int[] prices, int idx, int buy, int[][] dp) {
+        // base case
+        if (idx == prices.length) {
+            return 0;
+        }
+
+        if (dp[idx][buy] != -1) {
+            return dp[idx][buy];
+        }
+
+        int profit = 0;
+        if (buy == 1) {
+            // Buy or do not buy
+            profit = Math.max(-prices[idx] + func(prices, idx + 1, 0, dp), // buy
+                              0 + func(prices, idx + 1, 1, dp)); // do not buy
+        } else {
+            // Sell or do not sell
+            profit = Math.max(prices[idx] + func(prices, idx + 1, 1, dp), // sell
+                              0 + func(prices, idx + 1, 0, dp)); // do not sell
+        }
+
+        dp[idx][buy] = profit;
+        return dp[idx][buy];
     }
 }
