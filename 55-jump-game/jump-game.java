aa225f2e07ccb44e407a3d4f31cba8267daa2f8f
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-06-19 15:14:51 -0400

    Time: 1 ms (99.97%) | Memory: 45.5 MB (59.93%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -2,40 +2,6 @@ class Solution
 {
     public boolean canJump(int[] nums) 
     {
-        //Brute Force
-
-        // if(nums.length==1) return true;
-        // if(nums[0]==0) return false;
-
-        // boolean[] arr=new boolean[nums.length];
-        // Arrays.fill(arr,false);
-
-        // arr[0]=true;
-
-        // for(int i=0;i<nums.length;i++)
-        // {
-
-        //     if(!arr[i]) continue;
-
-        //     int num=nums[i];
-
-        //     int j=i+1;
-
-        //     while(j<nums.length && num>0)
-        //     {
-        //         arr[j++]=true;
-
-        //         if(arr[nums.length-1]) return true;
-
-        //         num--;
-        //     }   
-        // }
-
-        // return false;   
-
-
-        //Optimised
-
         int finalIdx=nums.length-1;
 
         for(int i=nums.length-2;i>=0;i--)
