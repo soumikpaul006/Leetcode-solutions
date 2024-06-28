885e4f3e767ad39fc04946982f03ebe2d124786d
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-06-27 21:50:27 -0400

    Time: 90 ms (7.03%) | Memory: 65.3 MB (32.97%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.java
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.java
@@ -1,25 +1,54 @@
-class Solution {
-    public long maximumImportance(int n, int[][] roads) 
+class Solution 
+{
+    class Pair
     {
+        int val;
+        int freq;
 
-        // degree edges of nodes
-        int[] degree = new int[n];
-        for (int[] road : roads) 
+        Pair(int val,int freq)
         {
-            degree[road[0]]++;
-            degree[road[1]]++;
+            this.val=val;
+            this.freq=freq;
+        }
+    }
+    public long maximumImportance(int n, int[][] roads) 
+    {   
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        for(int[] road:roads)
+        {
+            int u=road[0];
+            int v=road[1];
+
+            map.put(u,map.getOrDefault(u,0)+1);
+            map.put(v,map.getOrDefault(v,0)+1);
         }
 
-        // assign larger value to larger degree node
-        Arrays.sort(degree);
-        long result = 0;
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->b.freq-a.freq);
 
-        for (int i = 0; i < n; i++) 
+        HashMap<Integer,Integer> map2=new HashMap<>();
+
+        for(int key:map.keySet())
         {
-            result += (long) degree[i] * (i + 1);
+            pq.add(new Pair(key,map.get(key)));
         }
-        return result;
-        
-        
+
+        int num=n;
+
+        while(pq.size()>0)
+        {
+            Pair pair=pq.remove();
+            map2.put(pair.val,num--);
+        }
+
+        long total=0;
+
+        for(int[] road:roads)
+        {
+            total+=(map2.get(road[0])+map2.get(road[1]));
+        }
+
+        return total;
     }
 }
