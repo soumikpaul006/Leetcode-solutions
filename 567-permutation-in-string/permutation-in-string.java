00078d5bb6507fe636445edd420e0c6293b9cacf
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-06-28 19:02:24 -0400

    Time: 7 ms (70.56%) | Memory: 42.8 MB (47.26%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,53 +1,32 @@
 class Solution {
-    public boolean checkInclusion(String s1, String s2) 
-    {
-        if(s1.length()>s2.length()) return false;
-
-        int[] arr1=new int[26];
-
-        for(int i=0;i<s1.length();i++)
+        public boolean checkInclusion(String s1, String s2)
         {
-            arr1[s1.charAt(i)-'a']++;
-        }
-
-        // int[] arr2=new int[26];
-        
-
-
-        int l=0;
-        int r=s1.length()-1;
-
-        while(r<s2.length())
-        {
-            int[] arr2=new int[26];
-
-            for(int i=l;i<=r;i++)
+            if(s1.length() > s2.length()) return false;
+            
+            int[] arr1 = new int[26];
+            int[] arr2 = new int[26];
+            
+            for(int i = 0; i < s1.length(); i++)
             {
-                arr2[s2.charAt(i)-'a']++;
+                arr1[s1.charAt(i) - 'a']++;
+                arr2[s2.charAt(i) - 'a']++;
+            }
+            
+            if(Arrays.equals(arr1, arr2)) return true;
+            
+            int front = 0;
+            int back = s1.length();
+
+            while(back < s2.length())
+            {
+                arr2[s2.charAt(front) - 'a']--;
+                arr2[s2.charAt(back) - 'a']++;
+                
+                if(Arrays.equals(arr1, arr2)) return true;
+                front++;
+                back++;
             }
 
-            if(check(arr1,arr2))
-            {
-                return true;
-            }
-            l++;
-            r++;
+            return false;
         }
-
-        return false;
-
-        
-    }
-    public boolean check(int[] arr1,int[] arr2)
-    {
-        for(int i=0;i<26;i++)
-        {
-            if(arr1[i]!=arr2[i])
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
 }
