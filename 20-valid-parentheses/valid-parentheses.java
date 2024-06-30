f2ccace7a88639af57fcae9818108bb4754e7973
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-06-29 20:58:29 -0400

    Time: 2 ms (54.84%) | Memory: 41.6 MB (17.88%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) 
+    {
+        Stack<Character> st=new Stack<>();
+
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+
+            if(c=='('||c=='{'||c=='[')
+            {
+                st.add(c);
+            }
+
+            if(st.isEmpty()) return false;
+
+            if((c==')'&& st.pop()!='(') ||  (c=='}'&& st.pop()!='{') || (c==']'&& st.pop()!='['))
+            {
+                return false;
+            }
+
+        }
+        return st.isEmpty();
+    }
+}
