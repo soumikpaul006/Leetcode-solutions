90b0c70ebc4dc213fab8acc587b73bb01a17359d
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-07-01 22:23:21 -0400

    Time: 0 ms (100.00%) | Memory: 42.8 MB (52.65%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -3,23 +3,22 @@ class Solution {
     {
         int l=0;
         int r=nums.length-1;
+        int ans=0;
 
         while(l<=r)
         {
             int mid=(r+l)/2;
 
-            if(nums[mid]==target)
+            if(nums[mid]>=target) //may be ans answer
             {
-                return mid;
-            }
-            else if(target<nums[mid])
-            {
-                r=mid-1;
+                // ans=mid;
+                r=mid-1;//look for better options
             }
             else{
                 l=mid+1;
             }
         }
+
         return l;
         
     }
