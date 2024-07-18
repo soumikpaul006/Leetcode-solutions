475f5d33a00578be718d2edb57f476682023c306
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2024-07-18 18:11:21 -0400

    Time: 1 ms (82.78%) | Memory: 41.7 MB (83.77%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -5,27 +5,26 @@ class Solution
         List<List<Integer>> final_list=new ArrayList<>();
 
         final_list.add(new ArrayList<>());
-        final_list.get(0).add(1);  
+        final_list.get(0).add(1);//adding [1] initailly
 
         for(int i=1;i<numRows;i++)
         {
-            List<Integer> temp_list=new ArrayList<>();
-
-            temp_list.add(1);
-
             List<Integer> prev_list=final_list.get(i-1);
-
             int size=prev_list.size();
+
+            List<Integer> inner_list=new ArrayList<>();
+
+            inner_list.add(1);
 
             for(int j=1;j<size;j++)
             {
-                temp_list.add(prev_list.get(j-1)+prev_list.get(j));
+                inner_list.add(prev_list.get(j-1)+prev_list.get(j));
             }
 
-            temp_list.add(1);
-            final_list.add(temp_list);
+            inner_list.add(1);
 
-        } 
+            final_list.add(inner_list);
+        }
 
         return final_list;
     }
