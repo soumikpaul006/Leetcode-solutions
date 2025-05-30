e1486f9af287dc2b6c7a4da1140e684348c08e22
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2025-05-30 02:26:08 -0400

    Time: 15 ms (10.37%) | Memory: 44.9 MB (25.30%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,24 +1,25 @@
-class Solution {
+class Solution 
+{
     public boolean isValidSudoku(char[][] board) 
     {
         Set<String> set=new HashSet<>();
 
-        for(int i=0;i<9;i++)
+        for(int i=0;i<board.length;i++)
         {
-            for(int j=0;j<9;j++)
+            for(int j=0;j<board[i].length;j++)
             {
                 char c=board[i][j];
 
                 if(c!='.')
                 {
-                    if(!set.add(c+"in row"+i)||!set.add(c+"in col"+j)||!set.add(c+"in box"+i/3+j/3))
+                    if(!set.add("row"+c+i)||!set.add("col"+c+j)||!set.add("box"+c+i/3+j/3))
                     {
                         return false;
                     }
                 }
             }
         }
-        
-        return true;
+
+        return true;   
     }
 }
