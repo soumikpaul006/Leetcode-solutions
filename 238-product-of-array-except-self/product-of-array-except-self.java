0b044635fda16bcd143558724d00493b36f18de7
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2025-05-30 03:06:34 -0400

    Time: 678 ms (4.17%) | Memory: 58.6 MB (5.09%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,45 +1,41 @@
-class Solution {
+class Solution 
+{
     public int[] productExceptSelf(int[] nums) 
     {
         int[] left=new int[nums.length];
-        left[0]=1;
+        int[] right=new int[nums.length];
+        int[] ans=new int[nums.length];
 
-        for(int i=1;i<left.length;i++)
+        left[0]=1;
+    
+        for(int i=1;i<nums.length;i++)
         {
             left[i]=left[i-1]*nums[i-1];
         }
 
-        // for(int i=1;i<left.length;i++)
-        // {
-        //     System.out.print(left[i]+" ");
-        // }
+        for(int i=0;i<left.length;i++)
+        {
+            System.out.print(left[i]+" ");
+        }
 
-        int[] right=new int[nums.length];
-        right[right.length-1]=1;
+        right[nums.length-1]=1;
 
-        for(int i=right.length-2;i>=0;i--)
+        for(int i=nums.length-2;i>=0;i--)
         {
             right[i]=right[i+1]*nums[i+1];
         }
 
-        // System.out.println();
+        for(int i=0;i<right.length;i++)
+        {
+            System.out.print(right[i]+" ");
+        }
 
-        // for(int i=1;i<right.length;i++)
-        // {
-        //     System.out.print(right[i]+" ");
-        // }
-
-        int[] ans=new int[left.length];
-
-        for(int i=0;i<left.length;i++)
+        for(int i=0;i<nums.length;i++)
         {
             ans[i]=left[i]*right[i];
         }
 
         return ans;
-
-        
     }
 }
-//1   1    2    6
-//24  12   4    1
+
