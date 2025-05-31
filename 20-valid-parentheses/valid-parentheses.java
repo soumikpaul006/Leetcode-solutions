f494e3cc9e259c6196e243cc6e225a7282634a1f
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2025-05-31 02:02:19 -0400

    Time: 2 ms (97.19%) | Memory: 41.7 MB (66.00%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -7,19 +7,22 @@ class Solution {
         {
             char c=s.charAt(i);
 
-            if(c=='('||c=='{'||c=='[')
-            {
+            if(c=='(' || c=='{' || c=='['){ //opening
+
                 st.add(c);
+            }   
+            else{//closing
+                if(st.isEmpty()) return false;
+
+                char ch=st.pop();
+
+                if((c==')' && ch!='(' ) || (c=='}' && ch!='{') || (c==']' && ch!='['))
+                {
+                    return false;
+                }
             }
-
-            if(st.isEmpty()) return false;
-
-            if((c==')'&& st.pop()!='(') ||  (c=='}'&& st.pop()!='{') || (c==']'&& st.pop()!='['))
-            {
-                return false;
-            }
-
         }
+
         return st.isEmpty();
     }
 }
