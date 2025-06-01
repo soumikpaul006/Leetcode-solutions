9dae2134078d7b53e20595faf3fe5439bf07a7cc
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2025-06-01 17:20:47 -0400

    Time: 3 ms (6.78%) | Memory: 44.8 MB (18.97%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,29 +1,34 @@
 class Solution 
 {
-    public List<List<Integer>> permute(int[] nums) 
+    public List<List<Integer>> permute(int[] nums)
     {
         List<List<Integer>> final_list=new ArrayList<>();
-        backtrack(final_list,new ArrayList<>(),nums);
-        return final_list;     
+
+        backtrack(final_list,nums,new ArrayList<>());
+
+        return final_list;
     }
-    public void backtrack(List<List<Integer>> final_list, List<Integer> new_list, int[] nums)
+    public void backtrack(List<List<Integer>> final_list,int[] nums,List<Integer> temp_list)
     {
-        if(new_list.size()==nums.length)
+        if(temp_list.size()==nums.length)
         {
-            final_list.add(new ArrayList<>(new_list));
-            return;
+            final_list.add(new ArrayList<>(temp_list));
         }
 
         for(int i=0;i<nums.length;i++)
         {
-            if(new_list.contains(nums[i]))
+
+            if(temp_list.contains(nums[i]))
             {
                 continue;
-            } 
+            }
 
-            new_list.add(nums[i]);
-            backtrack(final_list,new_list,nums);
-            new_list.remove(new_list.size()-1);
+            temp_list.add(nums[i]);
+
+            backtrack(final_list,nums,temp_list);
+
+            temp_list.remove(temp_list.size()-1);
         }
+
     }
 }
