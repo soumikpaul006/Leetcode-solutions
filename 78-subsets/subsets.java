049cbda284e8e7e8fde2da6aa52bec1bb8f29125
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2025-06-01 16:30:51 -0400

    Time: 1 ms (51.81%) | Memory: 42.9 MB (46.32%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,43 +1,26 @@
-class Solution 
-{   
-    public List<List<Integer>> final_list=new ArrayList<>();
-    public List<List<Integer>> subsets(int[] nums)
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) 
     {
-        // List<List<Integer>> final_list=new ArrayList<>();
-        // final_list.add(new ArrayList<>());//add empty list
+        List<List<Integer>> final_list=new ArrayList<>();
 
-        // //iterate over each number
-        // for(int num:nums)
-        // {
-        //     int size=final_list.size();
-        //     for(int i=0;i<size;i++)
-        //     {
-        //         //make a copy the previous list
-        //         List<Integer> new_list=new ArrayList<>(final_list.get(i));
-        //         //add num to each list
-        //         new_list.add(num);
-        //         //add the new_list to the final_list
-        //         final_list.add(new_list);
-        //     }
-        // }
-        // return final_list;
+        backtrack(final_list,nums,new ArrayList<>(),0);
 
-        //backtracking approach
-        backtrack(nums,new ArrayList<>(),0);
         return final_list;
-
     }
-    public void backtrack(int[] nums,List<Integer> new_list,int idx)
+    public void backtrack(List<List<Integer>> final_list,int[] nums,List<Integer> temp_list,int idx)
     {
-        final_list.add(new ArrayList<>(new_list));
 
         for(int i=idx;i<nums.length;i++)
         {
-            new_list.add(nums[i]);
-            backtrack(nums,new_list,i+1);
-            new_list.remove(new_list.size()-1);
+            temp_list.add(nums[i]);
+
+            backtrack(final_list,nums,temp_list,i+1);
+
+            temp_list.remove(temp_list.size()-1);
+            
         }
+
+        final_list.add(new ArrayList<>(temp_list));
+
     }
 }
-//TC: O(n*2^n)
-//TcC: O(n*2^n)
