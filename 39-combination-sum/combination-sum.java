5d88f0ca4850ecb6d3a7ea7364035e921efffd79
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2025-06-01 17:57:40 -0400

    Time: 2 ms (83.70%) | Memory: 44.7 MB (63.97%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,40 +1,29 @@
-class Solution 
-{
-    public List<List<Integer>> combinationSum(int[] candidates, int target)
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) 
     {
         List<List<Integer>> final_list=new ArrayList<>();
-        backtrack(final_list,new ArrayList<>(),candidates,0,target,0);
+
+        backtrack(final_list,new ArrayList<>(),candidates,target,0,0);
+
         return final_list;
     }
-    public void backtrack(List<List<Integer>> final_list, List<Integer> new_list,int[] arr,int sum,int target, int idx)
+    public void backtrack(List<List<Integer>> final_list,List<Integer> temp_list,int[] nums,int target,int sum,int idx)
     {
-        //base
         if(sum==target)
         {
-            final_list.add(new ArrayList<>(new_list));
+            final_list.add(new ArrayList<>(temp_list));
             return;
         }
 
-        for(int i=idx;i<arr.length;i++)
+        for(int i=idx;i<nums.length;i++)
         {
-            if(sum + arr[i] <= target) 
-            {
-                new_list.add(arr[i]);
-                backtrack(final_list,new_list,arr,sum+arr[i],target,i);// Note: i instead of i + 1
-                new_list.remove(new_list.size()-1);
-            }
+            if(sum<=target)
+            {   
+                temp_list.add(nums[i]);
+                backtrack(final_list,temp_list,nums,target,sum+nums[i],i);
+                temp_list.remove(temp_list.size()-1);
+            }   
         }
+
     }
 }
-//TC: O(2^n)
-
-        //                   []
-        //          /      /    |    \     
-        //         2      3     6     7
-        //       /|\     / \    |
-        //      2 3 6 7  3 6 7   6 7
-        //     / \
-        //    2  3
-        //   /
-        //  2
-        // we are repeting the numbers so we did i instead of i+1
