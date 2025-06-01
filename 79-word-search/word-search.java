0872a11cc3e82114da3269ffc2c25afc09e73c8c
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2025-06-01 18:29:04 -0400

    Time: 142 ms (61.06%) | Memory: 41.6 MB (63.33%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,48 +1,51 @@
 class Solution 
 {
-    public String str="";
     public boolean exist(char[][] board, String word) 
     {
         int row=board.length;
         int col=board[0].length;
+
         boolean[][] visited=new boolean[row][col];
 
         for(int i=0;i<row;i++)
         {
             for(int j=0;j<col;j++)
             {
-                if(dfs(board,word,visited,i,j,0))
+                if(board[i][j]==word.charAt(0))
                 {
-                    return true;
+                    if(dfs(board,word,i,j,visited,0))
+                    {
+                        return true;
+                    }
                 }
             }
         }
 
-        return false;   
+        return false;
     }
-    public boolean dfs(char[][] board,String word,boolean[][] visited,int i,int j,int k)
-    { 
-        if(i<0||j<0||i>board.length-1||j>board[0].length-1|| visited[i][j] || board[i][j]!=word.charAt(k))
+    public boolean dfs(char[][] board,String word,int i,int j,boolean[][] visited,int counter)
+    {
+        if(i<0 || i>board.length-1 || j<0 || j>board[0].length-1 || visited[i][j])
+        {
+            return false;
+        }
+
+        if(board[i][j]!=word.charAt(counter))
         {
             return false;
         }
 
         visited[i][j]=true;
 
-        if(k==word.length()-1)
-        {
-            return true;
-        }
+        if(counter==word.length()-1) return true;
 
-       
-        boolean found = dfs(board,word,visited,i+1,j,k+1)||
-                        dfs(board,word,visited,i-1,j,k+1)||
-                        dfs(board,word,visited,i,j-1,k+1)||
-                        dfs(board,word,visited,i,j+1,k+1);
+        boolean found=dfs(board,word,i+1,j,visited,counter+1)||
+        dfs(board,word,i-1,j,visited,counter+1)||
+        dfs(board,word,i,j-1,visited,counter+1)||
+        dfs(board,word,i,j+1,visited,counter+1);
 
         visited[i][j]=false;
 
         return found;
-
     }
 }
