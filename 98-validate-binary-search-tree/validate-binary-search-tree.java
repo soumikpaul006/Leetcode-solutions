6f33027f3afebff0eaf1257fea5702b880e9b63e
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2025-06-01 22:52:48 -0400

    Time: 0 ms (100.00%) | Memory: 43.3 MB (79.03%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -13,23 +13,23 @@
  *     }
  * }
  */
-class Solution 
-{
-    
-    public boolean isValidBST(TreeNode root) 
-    {
-        return helper(root,Long.MIN_VALUE,Long.MAX_VALUE);
-    }
-    public boolean helper(TreeNode root,long min,long max)
-    {   
-        if(root==null) return true;
+class Solution {
+    public boolean isValidBST(TreeNode root) {
 
-        if(root.val<=min || root.val>=max)
+        return isValid(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+    public boolean isValid(TreeNode root,long start,long end)
+    {
+        if(root==null)
+        {
+            return true;
+        }
+
+        if(root.val<=start || root.val>=end)
         {
             return false;
         }
 
-        return helper(root.left,min,root.val) && helper(root.right,root.val,max);
+        return isValid(root.left,start,root.val)&&isValid(root.right,root.val,end);
     }
-
 }
