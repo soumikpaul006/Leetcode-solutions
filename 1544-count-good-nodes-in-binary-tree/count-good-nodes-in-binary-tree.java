989ea4cdd6649b902e693f50e77f5e4a8d63eead
Soumik Paul <64645812+soumikpaul006@users.noreply.github.com>
2025-06-01 23:11:57 -0400

    Time: 2 ms (84.91%) | Memory: 52.2 MB (87.30%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -13,27 +13,28 @@
  *     }
  * }
  */
-class Solution 
-{
+class Solution {
     int count=0;
     public int goodNodes(TreeNode root) 
-    {   
-        
+    {
         dfs(root,root.val);
         return count;
+        
     }
-    public void dfs(TreeNode root,int max)
+    public void dfs(TreeNode root,int parent)
     {
-
-        if(root==null) return;
-
-        if(root.val>=max)
+        if(root==null)
         {
-            count=count+1;
-            max=root.val;
+            return;
         }
 
-        dfs(root.left,max);
-        dfs(root.right,max);
+        if(root.val>=parent)
+        {
+            count++;
+            parent=root.val;
+        }
+
+        dfs(root.left,parent);
+        dfs(root.right,parent);
     }
 }
